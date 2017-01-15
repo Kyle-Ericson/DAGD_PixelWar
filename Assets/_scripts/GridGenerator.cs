ff967a15cb3262b102b93c7fc6603394f73643d6
Kyle Ericson <Kyle.ericson2@gmail.com>
2017-01-15 16:17:38 -0500

    Setup

---
+++ Assets/_scripts/GridGenerator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class GridGenerator
+{
+
+    /// <summary>
+    /// This creates the grid and returns a vector2 array of the locations.
+    /// </summary>
+    public static Vector2[,] GenerateGrid(int cols, int rows, Vector2 position)
+    {
+        Vector2[,] grid = new Vector2[cols,rows];
+
+        for (int x = 0; x < cols; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+
+                Vector2 cell = new Vector2(x + position.x, y + position.y);
+                grid[x,y] = cell;
+
+            }
+        }
+        return grid;
+    }
+}
---
+++ Assets/_scripts/MapMaker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapMaker : MonoBehaviour
+{
+    public GameObject tile;
+    
+
+
+    void Start()
+    {
+        SpawnMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnMap()
+    {
+        Vector2[,] grid = GridGenerator.GenerateGrid(20, 10, Vector2.zero);
+
+
+        for (var x = 0; x < grid.GetLength(0); x++)
+        {
+            for (var y = 0; y < grid.GetLength(1); y++)
+            {
+
+                var newTile = Instantiate(tile);
+
+                newTile.transform.position = grid[x,y];
+            }
+
+        }
+        
+    }
+}
