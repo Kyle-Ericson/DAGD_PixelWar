3a13aa0296d8520e79ed31c6eeeb3bcb71efbe5d
Kyle Ericson <Kyle.ericson2@gmail.com>
2017-05-29 07:18:03 -0400

    Created LevelData and add functionality to load a level based on its level data.

---
+++ Assets/_scripts/LevelData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LevelData {
+
+	// level 1
+	public static int[,] level_1 =
+	{
+		{1,1,1,1,1},
+		{1,1,1,1,1},
+		{1,1,2,1,1},
+		{1,1,1,1,1},
+		{1,1,1,1,1}
+	};
+	// level 2
+	public static int[,] level_2 =
+	{
+		{2,2,1,1,1,1,1,1},
+		{1,2,2,1,1,1,1,1},
+		{1,1,2,1,1,1,1,1},
+		{1,1,2,1,1,1,1,1},
+		{1,1,2,2,2,2,2,1},
+		{1,1,1,1,1,1,2,2}
+	};
+
+
+
+	/// <summary>
+	/// Returns the level data.
+	/// </summary>
+	public static int[,] Load(int level) {
+		switch(level) {
+			case 1: return level_1;
+			case 2: return level_2;
+		}
+		return null;
+	}
+}
---
+++ Assets/_scripts/LevelLoader.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject tile;
+    public GameObject tile2;
+
+    private int[,] level;
+    
+
+    /// <summary>
+	/// 
+	/// </summary>
+    void Start()
+    {
+        level = LevelData.Load(2);
+        if(level != null) SpawnLevel();
+    }
+    /// <summary>
+	/// 
+	/// </summary>
+    public void SpawnLevel()
+    {
+        float tileWidth = tile.transform.localScale.x; // tile's width
+        float tileHeight = tile.transform.localScale.y; // tile's height
+        float cols = level.GetLength(1); // the number of columns
+        float rows = level.GetLength(0); // the number of rows
+        Vector2 offset = new Vector2((cols * tileWidth * -0.5f) + (tileWidth / 2 ), (rows * tileHeight * -0.5f) + (tileHeight / 2)); // this centers the map at 0,0
+
+
+        for (var y = 0; y < rows; y++)
+        {
+            for (var x = 0; x < cols; x++)
+            {
+                GameObject newTile;
+                switch(level[y,x])
+                {
+                    case 1:
+                        newTile = Instantiate(tile);
+                        newTile.transform.position = new Vector2(x * tileWidth + offset.x, -y * tileHeight + -offset.y);
+                        break;
+                    case 2:
+                        newTile = Instantiate(tile2);
+                        newTile.transform.position = new Vector2(x * tileWidth + offset.x, -y * tileHeight + -offset.y);
+                        break;
+                }
+            }
+        }
+    }
+}
